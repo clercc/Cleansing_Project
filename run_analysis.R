c0d3db5a8a19b451a65a47b4e6161e1b0018a448
Christian <clercc@uw.edu>
2017-03-07 20:40:30 -0800

    column_eatures <- read.table(unz(fpath, "UCI HAR Dataset/features.t

--- run_analysis.R
+++ run_analysis.R
@@ -4,76 +4,80 @@
 
 library(tidyr)
 library(dplyr)
+library(reshape2)
 
-fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-fpath <- "~/github/datasciencecoursera/CleanseProject.zip"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+fpath <- "C:/Users/Christian/github/datasciencecoursera/CleanseProject.zip"
 download.file(fileURL, fpath)
 
 ###############
 #       load data files
 ###############
-x_train <- read.table(unz(fpath, "UCI HAR Dataset/train/X_train.txt"))
-y_train <- read.table(unz(fpath, "UCI HAR Dataset/train/y_train.txt"))
+train_data <- read.table(unz(fpath, "UCI HAR Dataset/train/X_train.txt"))
+trainfeatures <- read.table(unz(fpath, "UCI HAR Dataset/train/y_train.txt"))
+trainsubject <- read.table(unz(fpath, "UCI HAR Dataset/train/subject_train.txt"))
 
-x_test <- read.table(unz(fpath, "UCI HAR Dataset/test/X_test.txt"))
-y_test <- read.table(unz(fpath, "UCI HAR Dataset/test/y_test.txt"))
+test_data <- read.table(unz(fpath, "UCI HAR Dataset/test/X_test.txt"))
+testfeatures <- read.table(unz(fpath, "UCI HAR Dataset/test/y_test.txt"))
+testsubject <- read.table(unz(fpath, "UCI HAR Dataset/test/subject_test.txt"))
 
-row_activity <- read.table(unz(fpath, "UCI HAR Dataset/activity_labels.txt"))
-col_features <- read.table(unz(fpath, "UCI HAR Dataset/features.txt"))
+activity_names <- read.table(unz(fpath, "UCI HAR Dataset/activity_labels.txt"))
+column_features <- read.table(unz(fpath, "UCI HAR Dataset/features.txt"))
+
+###
+#       Create nomenclature vectors
+###
+activity_names[,2] <- as.character(activity_names[,2])
+column_features[,2] <- as.character(column_features[,2])
+subset_features <- grep(".*mean.*|.*std.*", column_features[,2])
+subset_names <- column_features[subset_features,2]
+subset_names = gsub('-mean', ' Mean', subset_names)
+subset_names = gsub('-std', ' Std', subset_names)
+subset_names = gsub('[-()]', '', subset_names)
+
+
 
 ##############
-#       Add in Primary Key to join x & y datasets
+#       Add in Primary Key to join data & feature datasets
 ##############
-x_train_n <- x_train %>% mutate(ID = seq(1,7352))
-y_train_n <- y_train %>% mutate(ID = seq(1,7352))
-x_test_n <- x_test %>% mutate(ID = seq(7353,10299))
-y_test_n <- y_test %>% mutate(ID = seq(7353,10299))
+train_data <- train_data[subset_features]  %>% mutate(ID = seq(1,7352))
+trainfeatures <- trainfeatures %>% mutate(ID = seq(1,7352))
+
+test_data <- test_data[subset_features] %>% mutate(ID = seq(7353,10299))
+testfeatures <- testfeatures %>% mutate(ID = seq(7353,10299))
 
 ##############
-#       Merge train, test, and activities data 
+#       Merge datasets 
 ##############
-train <- inner_join(y_train_n, x_train_n, by = c("ID" = "ID"))
-train <- inner_join(train, row_activity, by = c("V1.x" = "V1"))
+train <- inner_join(trainfeatures, train_data, by = c("ID" = "ID"))
+train <- inner_join(train, activity_names, by = c("V1.x" = "V1"))
 
-test <- inner_join(y_test_n, x_test_n, by = c("ID" = "ID"))
-test <- inner_join(test, row_activity, by = c("V1.x" = "V1"))
+test <- inner_join(testfeatures, test_data, by = c("ID" = "ID"))
+test <- inner_join(test, activity_names, by = c("V1.x" = "V1"))
+
+subject <- rbind(trainsubject, testsubject)
 
 f_data <- merge(train, test, all = TRUE)
+f_data <- cbind(subject,f_data[3:82])
+
+## Re-arrange order
+f_data <- f_data[,c(1,81,2:80)]
+
 
 ##############
 #       Updating Column Headers
 ##############
-col_features <- as.list(col_features[,2])
-colnames(f_data)[3:563] <- col_features
 
-##############
-#       Exploring the data
-##############
-names(f_data)
-str(f_data)
+colnames(f_data) <- c("Subject", "Activity", subset_names)
 
-        #       Set V1.x to factors
-        f_data$V1.x <- as.factor(f_data$V1.x)
-
-summary(f_data)
-head(f_data)
-tail(f_data)
-
-############
-#       Extract measurements on the mean and standard deviation
-############
-col_avg <- apply(f_data[,c(3:563)], 2, mean)
-col_sd <- apply(f_data[,c(3:563)], 2, sd)
-
-
-
-
-
-
-
-
+#       Set to factors
+f_data$Activity <- factor(f_data$Activity, levels = activity_names[,1], labels = activity_names[,2])
+f_data$Subject <- as.factor(f_data$Subject)
 
 #############
 #       Taking mean and standard deviation of all the measurements by group
-group_avg <- apply(f_data[,c(3:563)], 2, function(x) tapply(x,f_data$V2.y,mean))
-group_sd <- apply(f_data[,c(3:563)], 2, function(x) tapply(x,f_data$V2.y,sd))
+#############
+f_data <- melt(f_data, ID = c("Subject", "Activity"))
+f_data_mean <- dcast(f_data, Subject + Activity ~ variable, mean)
+
+write.table(f_data_mean, "C:/Users/Christian/github/Cleansing_Project/tidy.txt", row.names = FALSE, quote = FALSE)
